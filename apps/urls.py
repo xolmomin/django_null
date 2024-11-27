6e28736c0b46016643e5507d7319bbf05c25d0c2
xolmomin <xolmomin@gmail.com>
2024-11-27 15:21:08 +0500

    url ni yana o'zgartirdik

--- apps/urls.py
+++ apps/urls.py
@@ -4,6 +4,6 @@ from apps.views import ProductCreateAPIView, CategoryListAPIView
 
 urlpatterns = [
     path('products', ProductCreateAPIView.as_view(), name='product-list'),
-    path('categories', CategoryListAPIView.as_view(), name='categories-list'),
+    # path('categories', CategoryListAPIView.as_view(), name='categories-list'),
 ]
 
